8519aa812418e67c0aca7478652036eb4cd532c7
thejustjust <thejustjust@gmail.com>
2020-07-01 19:47:03 -0500

    Update flights.js

--- logic/flights.js
+++ logic/flights.js
@@ -1,7 +1,10 @@
 function Flights() {
   calculateNumberOfFlights(numPass,flightCap){
-    
+    if(numPass % flightCap ==0){
+    let numFlights = numpass / flightCap;
+    }
     return numFlights;
   }
 }
-
+return {calculateNumberOfFlights};
+module.exports = Flights();
