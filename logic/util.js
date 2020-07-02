4ccd7c5257f9d17ed831494ca0fbe47a5197629d
thejustjust <thejustjust@gmail.com>
2020-07-02 00:15:53 -0500

    Update util.js

--- logic/util.js
+++ logic/util.js
@@ -1,4 +1,24 @@
 function Util() {
+    function calculateTotalDistributedPassengers(distributedPassengers) {
+        let totalDistributedPassengers = 0;
+        let value;
+        for (value in distributedPassengers) {
+           totalDistributedPassengers += distributedPassengers[value];
+        }
+        return totalDistributedPassengers;
+     }
+
+     function calculateTotalNumberOfPassengers(passengersArray) {
+        let totalNumberOfPassengers = 0;
+        let passengers;
+        for (passengers of passengersArray) {
+           totalNumberOfPassengers += passengers;
+        }
+        return totalNumberOfPassengers;
+     }
+
+     return {calculateTotalDistributedPassengers, calculateTotalNumberOfPassengers};
 
 }
 
+module.exports = Util();
