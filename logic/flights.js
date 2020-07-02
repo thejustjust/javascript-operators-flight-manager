14f0dea241a78519c28eb0ef4c7be57322bdbac0
thejustjust <thejustjust@gmail.com>
2020-07-01 23:37:48 -0500

    Update flights.js

--- logic/flights.js
+++ logic/flights.js
@@ -1,8 +1,17 @@
 function Flights() {
   calculateNumberOfFlights(passengers,flightCap){
+    let flights;
+    
+    if ((passengers < 0) || (!Number.isInteger(Number(passengers)))) {
+         throw new Error("The number of passengers must be a positive integer value")
+    }
+
+    if ((capacity < 0) || (!Number.isInteger(Number(capacity)))) {
+      throw new Error("The capacity of the flight must be a positive integer value")
+    }
     
     if(passengers % flightCap ==0){
-    let flights = passengers / flightCap;
+    flights = passengers / flightCap;
     } else {
     flights = (passengers / flightCap) + 1;
     }
